f4b76a9f93b188e5545fbf0c2d30fd5af86c0332
Tannu2029 <Tannu2k29@gmail.com>
2026-08-13 00:22:11 +0530

    fix tax switch toggle styling and alignment

--- init/index.js
+++ init/index.js
@@ -1,28 +1,36 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "../.env" });
+}
+
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const Mongo_URL = "mongodb://127.0.0.1:27017/StaySphere";
-
-main()
-   .then(() => {
-    console.log("Connected to DB!");
-   }).catch(err => {
-    console.log(err);
-   });
+const dbUrl = process.env.ATLASDB_URL;
 
 async function main() {
-    await mongoose.connect(Mongo_URL);
+  await mongoose.connect(dbUrl);
 }
 
+main()
+  .then(() => {
+    console.log("Connected to Atlas DB!");
+    initDB();
+  })
+  .catch((err) => {
+    console.log("DB Connection Error:", err);
+  });
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    initdata.data = initdata.data.map((obj) => ({
-        ...obj,
-        owner: "6a7847a0027ecc3439c329cc",
-    }));
-    await Listing.insertMany(initdata.data);
-    console.log("data is initialised!");
-}
-initDB();
+  await Listing.deleteMany({});
+  initdata.data = initdata.data.map((obj) => ({
+    ...obj,
+    owner: "6a7847a0027ecc3439c329cc", 
+  }));
+  await Listing.insertMany(initdata.data);
+  console.log("Data is initialised in Atlas DB!");
+};
